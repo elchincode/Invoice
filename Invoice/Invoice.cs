e182d90d9de330f36772304e9acb81185261966f
elchincode <elchinai@code.edu.az>
2023-01-30 14:05:26 +0400

    Invoice

---
+++ Invoice/Invoice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVOICE
+
+{
+    public class Invoice
+    {
+        private string account;
+        private string customer;
+        private string provider;
+
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set;}
+
+
+        public  Invoice(string account, string customer, string provider)
+
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+
+        }
+
+        public decimal CostCalculation(bool needEdv)
+        {
+            if (needEdv == true)
+            {
+
+                decimal edv = 0.18m;
+                decimal price = Price * Quantity + (Price * edv);
+                return price;
+
+            }
+            else
+            {
+                return Price * Quantity;
+            }
+
+
+        }
+
+    }
+}
---
+++ Invoice/Program.cs
@@ -0,0 +1,31 @@
+﻿using INVOICE;
+using System;
+
+namespace ClassWork
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Invoice inv = new Invoice("678904", "Alex", "Foxtrot");
+            inv.Article = "USB-hab";
+            inv.Quantity = 6;
+            inv.Price = 30;
+            Console.WriteLine(inv.CostCalculation(true)); 
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
+
